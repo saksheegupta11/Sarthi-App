71fb2392190349cc5d90a7f5eec0fd3625f845f0
saksheegupta11 <saksheegupta520@gmail.com>
2026-05-10 12:12:30 +0530

    connected client with server

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env['CLIENT_URL'] || '*',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
